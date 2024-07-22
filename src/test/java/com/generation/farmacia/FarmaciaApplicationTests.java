3b67294db39d29e807dc84582eacb81d61526483
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-22 10:39:00 -0300

    configuração do projeto.

--- src/test/java/com/generation/farmacia/FarmaciaApplicationTests.java
+++ src/test/java/com/generation/farmacia/FarmaciaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FarmaciaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
